c9c5ca874449f5e529ab96406979391af3f6dbb8
Vernie <vernie.oliveiro@gmail.com>
2020-12-19 15:51:31 +0800

    successfully generated questions and randomised answer options

--- app.js
+++ app.js
@@ -31,69 +31,71 @@ const promise = $.ajax({
                      let question = item["question"]
                      let correctAnswer = item["correct_answer"]
                      let wrongAnswers = item["incorrect_answers"]
-                     // console.log (question)
-                     // console.log (correctAnswer)
-                     // for (item of wrongAnswers) {
-                     //        console.log (item)
-                     // }
 
                      //create a container to hold each question and possible answers
 
                      let questionDiv = $('<div>').addClass('question-container')
 
-                     //display questions
-                     let questionP = $('<p>').addClass("question-text").attr('id', `qntext${i+1}`)
-                     questionP.text(question)
+                     let answerDiv = $('<div>').addClass('answer-container')
 
-                     //display correct answer
-
-                     let correctAnswerP = $('<p>').addClass("answers-text correct").attr('id', `correct`).text(correctAnswer)
-
+                     //create element for the question and append it to the question-container
+                     let questionP = $('<p>').addClass("question-text").attr('id', `qntext${i+1}`).text(question)
                      questionDiv.append(questionP)
-                     questionDiv.append(correctAnswerP)
 
-                     //display wrong answers 
+                     //create an Array to host the possble answers
 
+                     let possibleAnswers=[ ]
+
+                     //create element for the correct answer
+                     //push correctAnswerP to possibleAnswers
+
+                     let correctAnswerP = $('<p>').addClass("answers-text correct").attr('id', `correct`).text(correctAnswer);
+
+                     possibleAnswers.push(correctAnswerP)
 
                      for (let item of wrongAnswers) {
-                            $('<p>').addClass("answers-text wrong"). text(item);
-                            questionDiv.append(item)
+                            possibleAnswers.push($('<p>').addClass("answers-text wrong"). text(item))
 
                      }
 
+                     //possibleAnswers is now an array of elements with all the possible answers
+                     console.log (possibleAnswers)
 
-                      
+                     //randomly display each element in possibleAnswers 
+
+                     //first shuffle possibleAnswers
+
+                     let currentIndex = possibleAnswers.length, temporaryValue, randomIndex;
                      
+                     // While there remain elements to shuffle...
+                     while (0 !== currentIndex) {
+                     
+                            // Pick a remaining element...
+                            randomIndex = Math.floor(Math.random() * currentIndex);
+                            currentIndex -= 1;
+                     
+                            // And swap it with the current element.
+                            temporaryValue = possibleAnswers[currentIndex];
+                            possibleAnswers[currentIndex] = possibleAnswers[randomIndex];
+                            possibleAnswers[randomIndex] = temporaryValue;
+                     }
+              
+              for (item of possibleAnswers){
+                     answerDiv.append(item)
+              }
+                     questionDiv.append(answerDiv)
 
                      $('main').append(questionDiv)
 
-                     
-
-
               }
 
-              //
+              
+       
 
-              //assigning span IDs for display
-
-              // display questions 
-
-              console.log (dataResourceMCQ.length)
-
-
-              for (let i=0; i<dataResource.length; i++) {
-                     let item =dataResource[i]
-                     $(`${item.questionSpanID}`).text(item.question)
-              }
-
-              // //read values from input fields 
-
-              // let answer1 = $('#answer1').value
-
-              // console.log (answer1)
+  
               
 
-              // $('.display-results').text(JSON.stringify(data["results"]))
+               
        },
        ()=>{
               console.log('bad request');
